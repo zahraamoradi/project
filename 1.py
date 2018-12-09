0a528fba9fc671fb036c3fb62994d70c0ed487b1
Zahra Moradi <zahraaa.moradi78@gmail.com>
2018-12-09 09:10:34 +0330

    add the project

---
+++ 1.py
@@ -0,0 +1,87 @@
+a = 2
+b = 3
+c = a+b
+print(c)
+a=2
+c=2*a
+print(c)
+a=2
+c=a*a
+print(c)
+print('2+2')
+print(2+2)
+
+print(type(2+2))
+print(type('zahra'))
+
+a = 12.5
+print(type(a))
+print(40/60)
+a='hello'
+b='zahra'
+print(a+b)
+import math
+print(math.sin(30))
+# print(range(1,10))
+
+for i in range(1,10):
+	print(i)
+
+def zahra(x):
+	print('salam')
+	print(x)
+
+
+zahra(2)
+def masoud(a,b,c):
+	m=a+b
+	j=c-b
+	print(m,j)
+
+masoud(3,4,5)
+m=30
+j=40
+print(m,j)
+x=-1
+if x<0:
+	print (x,"is negetive")
+else:
+    print (x,"is positive")
+
+def saba(x):
+	if x<0:
+		print(x,'is negetive')
+	else:
+	    if x>0:
+	        print(x,'is positive')
+	    else:
+	        print(x,'is ziro')
+
+
+saba(10)
+saba(-3)
+saba(0)	
+
+x=1
+
+if x>0 and x!=0:
+	print(x,'is positive')
+
+def sara(x):
+	if x<=0:
+	    print(' please enter a number greater than 0')
+	    return
+	if x%2==0:
+	    print(x,'is even')
+	else:
+	    print(x, 'is odd')
+
+sara(15)
+sara(20)
+print('\'')
+a='zahra'
+b=3*a
+print(zahra)
+b = 7
+c = a+b 
+print(c)
